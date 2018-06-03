5bd7e3f6aaa787e39515ff95c7223f262ffacb4b
Lucas Pettit <lpettit86@gmail.com>
2018-06-02 20:30:02 -0700

    adding new gold scrub
    reads jsons dumped by new silver scrub for boundingbox information, tracks the faces, smooths the tracking box, crops images, and saves to file

---
+++ goldDataScrub.py
@@ -0,0 +1,89 @@
+from Utils import *
+import os
+import json
+import cv2
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('--meta-src', type=str, required=True, help='Directory for .json silver data')
+parser.add_argument('--gif-src', type=str, required=True, help='Directory for gif files')
+parser.add_argument('--dest', type=str, required=True, help='Directory to save the snips')
+parser.add_argument('--target-dim', type=int)
+args = parser.parse_args()
+
+
+if args.target_dim is not None:
+    if args.target_dim <= 0:
+        raise ValueError('target-dim <= 0')
+if not os.path.isdir(args.meta_src):
+    raise NotADirectoryError('src path (%s) is not a directory' % args.meta_src)
+if not os.path.isdir(args.dest):
+    os.makedirs(args.dest)
+
+resdir = 'res'
+dataset_name = os.path.basename(args.dest)
+configpath = os.path.join(resdir, 'goldDataScrubConfig-%s.json' % dataset_name)
+if args.target_dim is not None:
+    target_dim = args.target_dim, args.target_dim
+else:
+    target_dim = None
+
+# load/build config object
+if not os.path.isfile(configpath):
+    config = {'start_index': 0}
+else:
+    with open(configpath) as f:
+        config = json.load(f)
+start_index = config['start_index']
+
+# get list of .json files from src
+files = [f for f in os.listdir(args.meta_src)]
+print('starting gold scrub')
+print('%d files found' % len(files))
+
+# iterate through files and perform post-processing
+for i, filename in enumerate(files[start_index:]):
+
+    # file_id
+    file_basename = os.path.splitext(filename)[0]
+
+    # log stuff
+    print('File - %s' % file_basename)
+
+    try:
+        gif = GIF.fromFile(os.path.join(args.gif_src, '%s.gif' % file_basename))
+        face_data = unpackJson(os.path.join(args.meta_src, filename))
+
+        people = {}
+
+        for person_id, fds in face_data.items():
+            # get nose coordinates and dimension vectors
+            nose_x = [fd.landmarks.center[0] for fd in fds]
+            nose_y = [fd.landmarks.center[1] for fd in fds]
+            dim = [fd.boundingBox.width for fd in fds]
+
+            # smoothify the movements
+            nose_smooth_x = smooth(nose_x, 5)
+            nose_smooth_y = smooth(nose_y, 5)
+            dim_smooth = smooth(dim, 5)
+
+            for fd, x, y, dim in zip(fds, nose_smooth_x, nose_smooth_y, dim_smooth):
+                # update the points
+                fd.centerOnPoint(x, y)
+                fd.expandRegionTo(dim, dim)
+
+                # get post-processed snip
+                snip = gif.getSnip(fd.frameId, fd.boundingBox)
+                if target_dim is not None:
+                    snip = cv2.resize(snip, target_dim, interpolation=cv2.INTER_CUBIC)
+
+                # write out the file
+                outfilename = os.path.join(args.dest, '%s_%d_%d.jpg' % (file_basename, fd.id, fd.frameId))
+                cv2.imwrite(outfilename, snip)
+    except Exception as e:
+        print(str(e))
+
+    # update config
+    config['start_index'] = i + 1
+    with open(configpath, 'w') as f:
+        json.dump(config, f)
